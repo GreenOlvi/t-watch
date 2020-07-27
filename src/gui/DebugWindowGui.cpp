50886ae8437a26135b00539deeb6e8740e1b21dc
Piotr Szulc <szulo123@gmail.com>
2020-07-27 19:34:03 +0200

    Properly clearing the line

--- src/gui/DebugWindowGui.cpp
+++ src/gui/DebugWindowGui.cpp
@@ -13,17 +13,16 @@ void DebugWindowGui::println(const char* text) {
 }
 
 void DebugWindowGui::draw() {
-    // _ttgo->tft->drawRect(_x, _y, _width, _height, TFT_GREEN);
-
     _ttgo->tft->setTextColor(TFT_GREEN, TFT_BLACK);
 
     for (int j = 0; j < _rows; j++) {
-        // _ttgo->tft->fillRect(_x + 1, _y + 4 + i * 10, _width - 2, 10, TFT_BLACK);
-        // _ttgo->tft->drawString(_lines[i], _x + 3, _y + 4 + i * 10, 1);
         for (int i = 0; i < _cols; i++) {
             char c = _debug->getChar(i, j);
-            _ttgo->tft->drawChar(c, _x + 3 + i * _charWidth, _y + 4 + j * _charHeight);
+            if (c == 0) {
+                _ttgo->tft->drawChar(' ', _x + 3 + i * _charWidth, _y + 4 + j * _charHeight);
+            } else {
+                _ttgo->tft->drawChar(c, _x + 3 + i * _charWidth, _y + 4 + j * _charHeight);
+            }
         }
     }
-
 }
